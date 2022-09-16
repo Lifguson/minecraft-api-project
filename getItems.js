58dd5207943b01914aa80c1edf36afb0f5eb6e58
Lifguson <lwellsteadgi@gmail.com>
2022-09-16 18:10:16 +0200

    finally got my fetch function to work!! :D

--- getItems.js
+++ getItems.js
@@ -1,14 +1,17 @@
+import { json } from "express";
 import fetch from "node-fetch";
 
-async function getItems(name) {
-  const response = await fetch(
-    "https://minecraft-ids.grahamedgecombe.com/items.json"
-  );
+async function getItems(url) {
+  const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error(`Error! status: ${response.status}`);
+  if (response.status == 200) {
+    let data = await response.json();
+    return data;
   }
-  const data = await response.json();
+  throw new Error(response.status);
 }
+
+const items = await getItems("https://minecraft-ids.grahamedgecombe.com/items.json");
+console.log(items);
 
 export default getItems;
