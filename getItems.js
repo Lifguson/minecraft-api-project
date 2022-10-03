8b89801d32c26cae7274de3ea916557bf275654a
Lifguson <lwellsteadgi@gmail.com>
2022-10-03 22:08:28 +0200

    trying to incorporate a second API call

--- getItems.js
+++ getItems.js
@@ -1,14 +1,28 @@
 import { json } from "express";
 import fetch from "node-fetch";
 
+
 async function getItems() {
   const response = await fetch("https://minecraft-ids.grahamedgecombe.com/items.json");
 
-  if (response.status == 200) {
     let result = await response.json();
     return result;
-  }
-  throw new Error(response.status);
 }
+  
+  
+  // const urls = [
+  //   "https://minecraft-ids.grahamedgecombe.com/items.json",
+  //   "https://minecraft-ids.grahamedgecombe.com/entities.json"
+  // ]
+
+  // try {
+    //   const response = await Promise.all(
+    //     urls.map(url => fetch(url).then(response => response.json()))
+    //   )
+    //   return response;
+    // } catch (err) {
+    //   console.log(err);
+    // }
+
 
 export default getItems;
